1ecae1e34ab234f1d68a00cc9521723effabc1a4
Ronan Michel <ronan.docilio.santos@everis.com>
2021-07-07 18:49:00 -0300

    Criação dos componentes de input, button e ancoras, definição dos estilos desses componentes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,61 @@
 import React from 'react';
-import {Title} from './styles';
+import {FiChevronRight} from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import {Title, Form, Repositories} from './styles';
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositórios no Github</Title>;
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer"/>
+            <Title>Explore repositórios no Github</Title>
+
+            <Form>
+                <input placeholder="Digite aqui o nome do repositório"/>
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="">
+                    <img 
+                    src="https://avatars.githubusercontent.com/u/71500239?v=4"
+                    alt="Ronan Michel"
+                    />
+                    <div>
+                        <strong>RonanMichel96/primeiro-projeto-react</strong>
+                        <p>Meu primeiro projeto de React</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+                <a href="">
+                    <img 
+                    src="https://avatars.githubusercontent.com/u/71500239?v=4"
+                    alt="Ronan Michel"
+                    />
+                    <div>
+                        <strong>RonanMichel96/primeiro-projeto-react</strong>
+                        <p>Meu primeiro projeto de React</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+                <a href="">
+                    <img 
+                    src="https://avatars.githubusercontent.com/u/71500239?v=4"
+                    alt="Ronan Michel"
+                    />
+                    <div>
+                        <strong>RonanMichel96/primeiro-projeto-react</strong>
+                        <p>Meu primeiro projeto de React</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
