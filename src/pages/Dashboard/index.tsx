da1a6e806459518b51743e3e9bb14452953c54f7
Ronan Michel <ronan.docilio.santos@everis.com>
2021-07-10 22:06:05 -0300

    Tratativa de erros, e criação de span quando um erro ocorrer

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,7 +4,7 @@ import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
-import {Title, Form, Repositories} from './styles';
+import {Title, Form, Repositories, Error} from './styles';
 interface Repository {
    full_name: string;
    description: string;
@@ -17,6 +17,7 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
     const [newRepo, setNewRepo]= useState('');
+    const [inputError, setInputError]= useState('');
     const [repositories, setRepositories]= useState<Repository[]>([]);
 
     /**
@@ -25,13 +26,27 @@ const Dashboard: React.FC = () => {
      * Salvar novo repositório no estado
      */
     async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
-        event.preventDefault();
-        
-        const response= await api.get<Repository>(`repos/${newRepo}`);
-        const repository = response.data;
 
-        setRepositories([...repositories, repository]);
-        setNewRepo('');
+        /**
+         * Para não ter reload da página.
+         */
+        event.preventDefault();
+
+        if(!newRepo){
+            setInputError('Digite o autor/nome do repositório');
+            return;
+        }
+
+        try{
+            const response= await api.get<Repository>(`repos/${newRepo}`);
+            const repository = response.data;
+    
+            setRepositories([...repositories, repository]);
+            setNewRepo('');
+            setInputError('');
+        }catch{
+            setInputError('Erro na busca por esse repositório');
+        }
     }
 
     return (
@@ -47,6 +62,8 @@ const Dashboard: React.FC = () => {
                  />
                 <button type="submit">Pesquisar</button>
             </Form>
+
+            {inputError && <Error>{inputError}</Error>}
 
             <Repositories>
                 {repositories.map(repository => (
